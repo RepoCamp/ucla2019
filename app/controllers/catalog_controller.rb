79af3af8533dc78389ffd6075222b828dfec8ae3
maxwellbenjamin <mkadel@gmail.com>
2019-08-26 14:53:05 -0400

    Metadata lesson three.
    Add metadata to search results and facets.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -44,6 +44,7 @@ class CatalogController < ApplicationController
     config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
     config.add_facet_field solr_name("resource_type", :facetable), label: "Resource Type", limit: 5
     config.add_facet_field solr_name("creator", :facetable), limit: 5
+    config.add_facet_field solr_name("year", :facetable), label: "Year", limit: 5
     config.add_facet_field solr_name("contributor", :facetable), label: "Contributor", limit: 5
     config.add_facet_field solr_name("keyword", :facetable), limit: 5
     config.add_facet_field solr_name("subject", :facetable), limit: 5
@@ -85,6 +86,7 @@ class CatalogController < ApplicationController
     config.add_index_field solr_name("identifier", :stored_searchable), helper_method: :index_field_link, field_name: 'identifier'
     config.add_index_field solr_name("embargo_release_date", :stored_sortable, type: :date), label: "Embargo release date", helper_method: :human_readable_date
     config.add_index_field solr_name("lease_expiration_date", :stored_sortable, type: :date), label: "Lease expiration date", helper_method: :human_readable_date
+    config.add_index_field solr_name("year", :stored_searchable), label: "Year"
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
